bd300f0ef99f70fdf511e0d2db63880b6812455d
Sunphetcharaj <sunphetcharaj123@gmail.com>
2024-05-14 17:50:33 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Products from './pages/Products.js'
+import Home from './pages/Home.js'
+import Cart from './pages/Cart.js'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="./Products.js">Products</Link></li>
+            <li><Link to="/Home.js">Home</Link></li>
+            <li><Link to="/Cart.js">Cart</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+        <Route path="./Home.js" exact component={Home} />
+        <Route path="./Products.js" component={Products} />
+        <Route path="./Cart.js" component={Cart} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
